de2502b261986055e5799c62f1c5828613824b04
s1771071 <s1771071@d-13102-082.ku-kinds.konan-u.ac.jp>
2018-10-26 16:46:31 +0900

    剣のオブジェクト

---
+++ teamD/src/Project/Sword.java
@@ -0,0 +1,26 @@
+package Project;
+
+import framework.game2D.OvergroundActor2D;
+
+/**
+ * “G‚ÌƒNƒ‰ƒX
+ *
+ * @author H.Kotobuki
+ *
+ */
+public class Sword extends OvergroundActor2D {
+
+
+	@Override
+	public String getAnimationFileName() {
+		return null;
+
+	}
+
+	@Override
+	public String getModelFileName() {
+		//Œ•‚Ì‰æ‘œ“Ç‚İ‚İ
+		return "data\\Head4.wrl";
+	}
+
+}
